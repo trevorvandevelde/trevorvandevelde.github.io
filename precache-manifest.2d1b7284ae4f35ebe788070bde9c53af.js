608cc52cd6f6bef5f18379c60b72d00d2d6de704
Trevor Van de Velde <vantrevor@berkeley.edu>
2020-10-30 14:10:36 -0700

    attempt #2

---
+++ precache-manifest.2d1b7284ae4f35ebe788070bde9c53af.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51be53254e6a2a157b8374db612ff4cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "454896591b671ec8be9a",
+    "url": "/static/css/2.83cfaa1b.chunk.css"
+  },
+  {
+    "revision": "9a6a60c1398271683500",
+    "url": "/static/css/main.1219da1f.chunk.css"
+  },
+  {
+    "revision": "454896591b671ec8be9a",
+    "url": "/static/js/2.bd326b9a.chunk.js"
+  },
+  {
+    "revision": "7a6c2012de3b5633edfb7bcc194ab5f8",
+    "url": "/static/js/2.bd326b9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6a60c1398271683500",
+    "url": "/static/js/main.56219a8a.chunk.js"
+  },
+  {
+    "revision": "f9c64c206bee5e187420",
+    "url": "/static/js/runtime-main.ecdefcab.js"
+  },
+  {
+    "revision": "c3fa39de4934f10feb47c31d5124ab76",
+    "url": "/static/media/book.c3fa39de.jpg"
+  },
+  {
+    "revision": "394300fb9709f511afbd1014a42bda7c",
+    "url": "/static/media/cmd_palette.394300fb.png"
+  },
+  {
+    "revision": "4a461d17106a90a69225731447af0c05",
+    "url": "/static/media/profile.4a461d17.jpg"
+  },
+  {
+    "revision": "d7cff37777b7bffbc530ec981b598c88",
+    "url": "/static/media/scythe.d7cff377.jpg"
+  },
+  {
+    "revision": "3efe237b942f38961c690e7a8993a161",
+    "url": "/static/media/skating.3efe237b.jpg"
+  },
+  {
+    "revision": "e88a238556c9fb4a7e2a10af14e660f0",
+    "url": "/static/media/study_abroad.e88a2385.jpg"
+  }
+]);
